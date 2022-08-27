b946a1c1109ac6665477fd4b55b2b1f9a931fb60
Walter <walter.inacio28@gmail.com>
2022-08-26 22:27:51 -0300

    first commit

--- numberGenerator/ViewController.swift
+++ numberGenerator/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblNumber: UILabel!
+    
+    
+    @IBAction func generateNumber(_ sender: Any) {
+        let result = arc4random_uniform(30);
+        lblNumber.text = String(result);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
