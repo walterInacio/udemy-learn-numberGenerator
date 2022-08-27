b5b4c856477d0ac9f8cdfacb0dbd2865d908e9ac
Walter <walter.inacio28@gmail.com>
2022-08-26 21:44:17 -0300

    Initial Commit

---
+++ numberGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  numberGenerator
+//
+//  Created by Walter Filho on 26/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
